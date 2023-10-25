f4a7b9dcf4a855c90a6bfa8dda646aab18502b68
mtobin-tdab <110408564+mtobin-tdab@users.noreply.github.com>
2023-10-25 14:21:39 +0100

    better logging and dont try to grant permissions

--- octaipipeTemplate/artifacts/azure_setup.sh
+++ octaipipeTemplate/artifacts/azure_setup.sh
@@ -2,7 +2,7 @@
 set -e
 
 az login --identity -u /subscriptions/0376d230-c884-4b5d-80b2-6759120231fc/resourceGroups/arm-test-deploy5/providers/Microsoft.ManagedIdentity/userAssignedIdentities/ARM-deployer
-echo "Logged to to az with managed identity!"
+echo -e "\nLogged to to az with managed identity!"
 
 
 # Default values
@@ -17,12 +17,12 @@ while getopts ":c:s:t:u:" opt; do
     s) subscriptionId="$OPTARG" ;;
     t) tenantId="$OPTARG" ;;
     u) userEmails_str="$OPTARG" ;;
-    \?) echo "Invalid option -$OPTARG" >&2 ;;
+    \?) echo -e "\nInvalid option -$OPTARG" >&2 ;;
   esac
 done
 
 
-echo "Setting up AD for $customerName !"
+echo -e "\nSetting up AD for $customerName !"
 IFS=',' read -ra userEmails <<< "$userEmails_str"
 
 # Check user emails input is valid before running anything
@@ -35,13 +35,13 @@ do
   fi
 done
 if [ ${#notFoundEmails[@]} -ne 0 ]; then
-    echo "Error: The following emails were not found:"
+    echo -e "\nError: The following emails were not found:"
     for email in "${notFoundEmails[@]}"; do
-    echo "$email"
+    echo -e "\n$email"
     done
 exit 1
 fi
-echo "Email validation complete!"
+echo -e "\nEmail validation complete!"
 
 # Set Global Variables
 # readersAppRoleId=$(uuidgen)
@@ -49,7 +49,7 @@ readersAppRoleId=$readersAppRoleIdGuid
 
 # Set Azure Context
 az account set --subscription $subscriptionId
-echo "Azure context set!"
+echo -e "\nAzure context set!"
 
 # Create AppRoles
 appRoles="[
@@ -68,14 +68,14 @@ app=$(az ad app create --display-name "${customerName}-admin" --sign-in-audience
 objectId=$(az ad app list --filter "appId eq '$app'" --query "[].id" -o tsv)
 sp=$(az ad sp create --id $app --query id -o tsv)
 az ad sp update --id $sp --set "appRoleAssignmentRequired=true"
-echo "App and Service Principal created!"
+echo -e "\nApp and Service Principal created!"
 
 # Wait for propagation
 sleep 20
 
 # Assign Role
 az role assignment create --assignee $sp --role Owner --scope "/subscriptions/$subscriptionId"
-echo "Role assignment complete!"
+echo -e "\nRole assignment complete!"
 
 # Create App Role (assuming this part in json format)
 # TODO: Replace this section with actual commands for creating App Role
@@ -83,7 +83,7 @@ echo "Role assignment complete!"
 # Get service principal for Microsoft Graph
 graphSp=$(az ad sp list --filter "AppId eq '00000003-0000-0000-c000-000000000000'" --query "[0].id" -o tsv)
 graphRoles=$(az ad sp show --id $graphSp --query "appRoles" -o json)
-echo "Service principal for Microsoft Graph obtained!"
+echo -e "\nService principal for Microsoft Graph obtained!"
 
 applicationReadWriteRoleId=$(echo $graphRoles | jq -r '.[] | select(.value == "Application.ReadWrite.All") | .id')
 groupReadAllRoleId=$(echo $graphRoles | jq -r '.[] | select(.value == "Group.Read.All") | .id')
@@ -98,14 +98,14 @@ az ad app permission add --id $app --api 00000003-0000-0000-c000-000000000000 --
 az ad app permission add --id $app --api 00000003-0000-0000-c000-000000000000 --api-permissions e1fe6dd8-ba31-4d61-89e7-88639da4683d=Scope 2>/dev/null
 az ad app permission add --id $app --api 00000003-0000-0000-c000-000000000000 --api-permissions $userReadAllRoleId=Role 2>/dev/null
 az ad app permission add --id $app --api 00000003-0000-0000-c000-000000000000 --api-permissions $mailSendRoleId=Role 2>/dev/null
-echo "Permissions added!"
+echo -e "\nPermissions added!"
 
-# Grant permissions
-if az ad app permission admin-consent --id $app 2>/dev/null; then
-  echo "Permissions granted!"
-else
-  echo "Unable to grant permission - please get an administrator to do so."
-fi
+# # Grant permissions
+# if az ad app permission admin-consent --id $app 2>/dev/null; then
+#   echo -e "\nPermissions granted!"
+# else
+#   echo -e "\nUnable to grant permission - please get an administrator to do so."
+# fi
 
 
 # Setup Authentication
@@ -113,7 +113,7 @@ nativeClient='https://login.microsoftonline.com/common/oauth2/nativeclient'
 liveSDK='https://login.live.com/oauth20_desktop.srf'
 msalonly="msal$app://auth"
 az ad app update --id $app --public-client-redirect-uris "http://localhost" $nativeClient $liveSDK $msalonly
-echo "Authentication setup complete!"
+echo -e "\nAuthentication setup complete!"
 
 # Allow public client flows set to True 
 az rest --method PATCH --uri "https://graph.microsoft.com/v1.0/applications(appId='$app')" --headers 'Content-Type=application/json' --body '{"isFallbackPublicClient": true}'
@@ -121,7 +121,7 @@ az rest --method PATCH --uri "https://graph.microsoft.com/v1.0/applications(appI
 # User and Group Assignment
 groupName="${customerName}-octaipipe-users"
 groupId=$(az ad group create --display-name $groupName --mail-nickname "NotUsed" --description "Group For OctaiPipe Users" --query id -o tsv)
-echo "User and Group Assignment complete!"
+echo -e "\nUser and Group Assignment complete!"
 
 # Assign Service Principal as Owner
 az ad group owner add --group $groupId --owner-object-id $sp
@@ -133,7 +133,7 @@ appRolebody="{
   \"appRoleId\": \"$readersAppRoleId\"
 }"
 az rest --method POST --uri "https://graph.microsoft.com/v1.0/groups/$groupId/appRoleAssignments" --body "$appRolebody" --headers 'Content-Type=application/json'
-echo "New AppRoleAssignment for the group created!"
+echo -e "\nNew AppRoleAssignment for the group created!"
 
 # Assign Users
 for userEmail in "${userEmails[@]}"
@@ -141,11 +141,11 @@ do
   userId=$(az ad user list --query "[?mail=='$userEmail'].id | [0]" -o tsv)
   az ad group member add --group $groupId --member-id $userId
 done
-echo "Users assigned to group!"
+echo -e "\nUsers assigned to group!"
 
 # Create Client Secret
 clientSecret=$(az ad app credential reset --id $app --append --display-name "OctaiPipe Secret" --years 4 --query password -o tsv)
-echo "Client secret created!"
+echo -e "\nClient secret created!"
 
 # Create JSON output
 output_json="{
@@ -155,7 +155,7 @@ output_json="{
   \"objectId\": \"$objectId\",
   \"servicePrincipalSecret\": \"$clientSecret\"
 }"
-echo "JSON output created!"
+echo -e "\nJSON output created!"
 
 # Write JSON to AZ_SCRIPTS_OUTPUT_PATH
 echo $output_json > $AZ_SCRIPTS_OUTPUT_PATH
